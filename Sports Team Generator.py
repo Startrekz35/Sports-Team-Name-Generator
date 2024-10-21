dea76a0ad72db70f305fd1da533e5710639e6c0f
Startrekz35 <burkz3905@outlook.com>
2024-10-21 15:34:04 -0500

    My first Python Project

---
+++ Sports Team Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Sports Team name Generator")
+city = input("What's your favorite city?")
+animal = input("Now, what's your favorite animal?")
+print("Your sports team name could be: " + city + " " + animal + "s")
---
+++ readme.txt
@@ -0,0 +1,1 @@
+This is my first python project where I use string concatenation with a couple of input prompts to create the desired sports team name
